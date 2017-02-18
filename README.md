8489f2474b1f9fb608e4f868f67cf0065083f0cf
Patrick Dayton <pdayton@fastrobot.com>
2017-02-18 16:25:44 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# ruby_on_rails
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'ruby_on_rails'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures ruby_on_rails'
+long_description 'Installs/Configures ruby_on_rails'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/ruby_on_rails/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/ruby_on_rails' if respond_to?(:source_url)
